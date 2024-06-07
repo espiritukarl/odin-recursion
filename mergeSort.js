e892f6a48d8e0aa72ae861868d7d275cff390b2f
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-06-08 07:44:31 +0800

    Added solution for MergeSort

---
+++ mergeSort.js
@@ -0,0 +1,29 @@
+function mergeSort(arr) {
+    if (arr.length === 1 || arr.length === 0) return arr;
+    let tempLength = Math.floor(arr.length/2)
+    let left = arr.slice(0, tempLength)
+    let right = arr.slice(tempLength)
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] <= right[rightIndex]) {
+            result.push(left[leftIndex])
+            leftIndex++
+        } else if (left[leftIndex] > right[rightIndex]){
+            result.push(right[rightIndex])
+            rightIndex++
+        }
+    }
+
+    return [...result, ...left.slice(leftIndex), ...right.slice(rightIndex)];
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]))
+console.log(mergeSort([105, 79, 100, 110]))
