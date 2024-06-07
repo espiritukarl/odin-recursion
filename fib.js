9070caa13207a711a269f42b52b14bf64818c0b4
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-06-08 07:00:52 +0800

    Added solution to Assignment 1 - Fibonacci

---
+++ fib.js
@@ -0,0 +1,19 @@
+function fib(num) {
+    let result = []
+    for (let i = 0; i < num; i++) {
+        if (i === 0) result.push(i)
+        else if (i === 1) result.push(1)
+        else result.push(result[i-1] + result[i-2])
+    }
+    return result
+}
+
+console.log(fib(8))
+
+function fibsRec(num, arr = [0, 1]) {
+    if (arr.length >= num) return arr
+    arr.push(arr[arr.length-1]+arr[arr.length-2])
+    return fibsRec(num, arr)
+}
+
+console.log(fibsRec(10))
